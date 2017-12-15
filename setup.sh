cc93a385c21afe7bff1657f1551b6bdc13220d24
Roy <rhh@crosscompute.com>
2017-12-15 10:27:43 -0500

    Add notes on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 # Add shell commands here to configure your environment automatically
-# Check /tmp/setup.log to debug this script
+# Note that this script will be running in parallel with the notebook server
+# Run "cat /tmp/setup.log" in the notebook to check setup progress
 
 # pip install scikit-learn
